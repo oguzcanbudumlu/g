7437349606dda1ecdf150daad5df3388dfb6df42
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-25 17:46:58 +0300

    add circles

--- w-test/structs/struct_test.go
+++ w-test/structs/struct_test.go
@@ -1,6 +1,9 @@
 package structs
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -13,17 +16,33 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	r := Rectangle{12.0, 6.0}
-	got := Area(r)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		r := Rectangle{12.0, 6.0}
+		got := r.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
+}
+
+func (r Circle) Area() float64 {
+	return math.Pi * r.Radius * r.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -33,4 +52,8 @@ func Perimeter(rectangle Rectangle) float64 {
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
