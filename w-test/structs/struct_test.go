b2b1a07b9886b4f89098d6a938084fc909542efd
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-25 17:38:30 +0300

    refactor

--- w-test/structs/struct_test.go
+++ w-test/structs/struct_test.go
@@ -3,7 +3,8 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	r := Rectangle{12.0, 6.0}
+	got := Area(r)
 	want := 72.0
 
 	if got != want {
@@ -20,10 +22,15 @@ func TestArea(t *testing.T) {
 	}
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(r Rectangle) float64 {
+	return r.Height * r.Width
 }
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
