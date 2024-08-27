8c902dcf07b14be02ee14aa91d21ff969e96324f
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-27 08:25:24 +0300

    Add initial test

---
+++ w-test/concurrency/concurrency_test.go
@@ -0,0 +1,42 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+)
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"https://dave.cheney.net",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":       true,
+		"https://dave.cheney.net": true,
+		"waat://furhurterwe.geds": false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v got %v", want, got)
+	}
+}
