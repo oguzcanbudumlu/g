4abcdb17226f4d3ea67b5d36d9db770e20644167
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-27 08:30:00 +0300

    Add benchmark test

--- w-test/concurrency/concurrency_test.go
+++ w-test/concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 type WebsiteChecker func(string) bool
@@ -38,5 +39,21 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
