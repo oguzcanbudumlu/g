d0f9cd14503258085924a1d0f2e24dc638966d17
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-27 20:37:48 +0300

    Add test with httpserver

--- w-test/select/select_test.go
+++ w-test/select/select_test.go
@@ -2,11 +2,12 @@ package racer
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-func TestRacer(t *testing.T) {
+func TestRacerFlaky(t *testing.T) {
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.dev"
 
@@ -16,6 +17,30 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+}
+
+func TestRacerWithServer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
 
 func Racer(a string, b string) (winner string) {
