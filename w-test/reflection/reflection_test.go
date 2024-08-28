fee51b1e5ec8eb9a851f6defd23f34a1724c55e0
oguzcanbudumlu <obudumlu@gmail.com>
2024-08-28 20:09:59 +0300

    Add first reflection test

---
+++ w-test/reflection/reflection_test.go
@@ -0,0 +1,29 @@
+package reflection
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	expected := "Fenerbahce"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+
+}
+
+func walk(x interface{}, fn func(input string)) {
+	fn("Fenerbahce")
+}
